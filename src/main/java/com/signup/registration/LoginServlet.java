9d31acc14ba665fa39425e3482f8d43e8fc7e0ba
Harshvardhan Gandharv <hvardhan.1602@gmail.com>
2025-07-24 12:11:57 +0530

    recompilation

--- src/main/java/com/signup/registration/LoginServlet.java
+++ src/main/java/com/signup/registration/LoginServlet.java
@@ -7,13 +7,12 @@ import java.sql.*;
 
 import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
-import javax.servlet.annotation.WebServlet;
+// import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 
-@WebServlet("/login")
 public class LoginServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
 
